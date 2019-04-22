1827b0510b18fa8b3d2a536638be7c5c44fb866e
sohie <lee.ellin@gmail.com>
2019-04-22 10:41:40 +0800

    codes

---
+++ Class03b/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Class03b
+//
+//  Created by sophie on 2019/4/22.
+//  Copyright © 2019 sophie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Class03bTests/Class03bTests.swift
@@ -0,0 +1,34 @@
+//
+//  Class03bTests.swift
+//  Class03bTests
+//
+//  Created by sophie on 2019/4/22.
+//  Copyright © 2019 sophie. All rights reserved.
+//
+
+import XCTest
+@testable import Class03b
+
+class Class03bTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
