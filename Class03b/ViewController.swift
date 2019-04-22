8ad4179164c00bfddafc21bcf5f750918aa15f07
sohie <lee.ellin@gmail.com>
2019-04-22 11:58:04 +0800

    0422-hw

--- Class03b/ViewController.swift
+++ Class03b/ViewController.swift
@@ -9,7 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var MessageA: UILabel!
+    
+    @IBAction func Click(_ sender: UIButton) {
+        MessageA.text = "Hi, Xcode"
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
