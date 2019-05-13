52bef5e7cc666e398c0b4d353a1aab7c3404bbba
sohie <lee.ellin@gmail.com>
2019-05-13 11:56:18 +0800

    20190513- hw

--- Class03b/MyWebViewController.swift
+++ Class03b/MyWebViewController.swift
@@ -7,27 +7,30 @@
 //
 
 import UIKit
+import WebKit
 
-class MyWebViewController: UIViewController {
+class MyWebViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var myWebView: WKWebView!
+    @IBOutlet weak var btnGoBottomConstraint: NSLayoutConstraint!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        myWebView.load(URLRequest(url: URL(string: "https://www.google.com")!))
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         
-        
-        
-        
-        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillAppear(notificaton:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillAppear(notification:)), name: UIResponder.keyboardWillShowNotification, object: nil)
         
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillDisAppear(notificaton:)), name: UIResponder.keyboardWillHideNotification, object: nil)
     }
     
-    override func viewDidDisappear(_ animated: Bool) {
+    override func viewWillDisappear(_ animated: Bool) {
         super.viewDidDisappear(animated)
         
         NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
@@ -35,15 +38,48 @@ class MyWebViewController: UIViewController {
         NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
     }
     
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    // MARK: - TextField
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        
+        let accept = "abcdeABCDE";
+        
+        let cs = NSCharacterSet(charactersIn: accept).inverted
+        
+        let filters = string.components(separatedBy: cs).joined(separator: "")
+        
+        if (string != filters)
+        {
+            return false
+        }
+        
+        let current = textField.text! as NSString
+        
+        let newString : NSString = current.replacingCharacters(in: range, with: string) as NSString
+        
+        return newString.length <= 10
+    }
+    
 // MARK: - keyboard
 
-    @objc func keyboardWillAppear(notificaton:NSNotification?) {
+    @objc func keyboardWillAppear(notification:NSNotification?) {
         print("keyboardWillApear")
+        
+        guard let frame = notification?.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue else {
+            return
+        }
+    //self.btnGoBottomConstraint.constant = 444;
+    self.btnGoBottomConstraint.constant = frame.cgRectValue.height;
     }
     
     @objc func keyboardWillDisAppear(notificaton:NSNotification?) {
         print("keyboardWillDisAppear")
-    }
+        
+    self.btnGoBottomConstraint.constant = 44;    }
     
     
     /*
