beed0c5e3c1c97bf56c84173a38984e7de46a551
sohie <lee.ellin@gmail.com>
2019-05-27 12:01:50 +0800

    2019/5/27 pod and sqllite

---
+++ Pods/Target Support Files/Pods-Class03bTests/Pods-Class03bTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Class03bTests : NSObject
+@end
+@implementation PodsDummy_Pods_Class03bTests
+@end
---
+++ Pods/Target Support Files/Pods-Class03bUITests/Pods-Class03bUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Class03bUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Class03bUITests
+@end
